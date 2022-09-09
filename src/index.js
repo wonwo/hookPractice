d46a63666a4d2983e353148828aa49552d70c813
wonwo <peacewalker58911@gmail.com>
2022-09-09 22:53:05 +0800

    b4 hook

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Game from './game';
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
